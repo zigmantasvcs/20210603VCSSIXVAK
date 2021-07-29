d78d8143cd6fece05734ba6a28280d08fefb24c8
Zigmantas <zigmantas@mrorangetech.se>
2021-07-29 18:45:50 +0300

    translation upgrade

--- paskaitos/js/pozicionavimasTranslation.js
+++ paskaitos/js/pozicionavimasTranslation.js
@@ -4,12 +4,27 @@ function doTranslations(language){
     // aprasysime vertimo logika
     //console.log(translation);
 
-    document.querySelector("[translate-menu-main]").innerText = translation.menu.main;
-    document.querySelector("[translate-menu-services]").innerText = translation.menu.services;
-    document.querySelector("[translate-menu-contact]").innerText = translation.menu.contact;
-    document.querySelector("[translate-menu-partners]").innerText = translation.menu.partners;
-    document.querySelector("[translate-menu-gallery]").innerText = translation.menu.gallery;
+    // document.querySelectorAll("[translate-menu-main]")[0].innerText = translation.menu.main;
+    // document.querySelectorAll("[translate-menu-main]")[1].innerText = translation.menu.main;
+    // document.querySelectorAll("[translate-menu-services]").innerText = translation.menu.services;
+    // document.querySelectorAll("[translate-menu-contact]").innerText = translation.menu.contact;
+    // document.querySelectorAll("[translate-menu-partners]").innerText = translation.menu.partners;
+    // document.querySelectorAll("[translate-menu-gallery]").innerText = translation.menu.gallery;
+
+    doElementsTranslation(document.querySelectorAll("[translate-menu-main]"), translation.menu.main);
+    doElementsTranslation(document.querySelectorAll("[translate-menu-services]"), translation.menu.services);
+    doElementsTranslation(document.querySelectorAll("[translate-menu-contact]"), translation.menu.contact);
+    doElementsTranslation(document.querySelectorAll("[translate-menu-partners]"), translation.menu.partners);
+    doElementsTranslation(document.querySelectorAll("[translate-menu-gallery]"), translation.menu.gallery);
 }
+
+function doElementsTranslation(elements, text){
+    for (let index = 0; index < elements.length; index++) {
+        const element = elements[index];
+        element.innerText = text;
+    }
+}
+
 
 function getLanguageTranslations(language){
     let translations = {
